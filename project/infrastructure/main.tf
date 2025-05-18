6eb4b0cbb31e0b3a704514251402e4d2955c46d6
Naimmo <samuel.haddar@baxenergy.com>
2025-05-18 16:38:54 +0200

    Refactor infrastructure: update .gitignore, replace AWS provider with Docker, and add Kubernetes resources for PostgreSQL

--- project/infrastructure/main.tf
+++ project/infrastructure/main.tf
@@ -1,23 +1,10 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.0"
     }
   }
-
-  required_version = ">= 1.2.0"
 }
 
-provider "aws" {
-  region  = "us-west-2"
-}
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
-}
+provider "docker" {}
